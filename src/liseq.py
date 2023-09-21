b0430873ab929aa9aaae2d8e1f4544faef117a13
Laura <lauramhfur@gmail.com>
2023-09-21 14:45:08 +0200

    CTiB Exercises Week 3

--- src/liseq.py
+++ src/liseq.py
@@ -31,4 +31,25 @@ def liseq(x: Sequence[Any]) -> list[int]:
     """
     best: list[int] = []
     # Explore all alternatives
+    """Finding increasing substrings"""
+    increasing_substrings = []
+    for i in range(len(x)-1):
+        accumulator = 0
+        for j in range(i+1, len(x)+1):
+            increasing = is_increasing(x[i+accumulator:j+1])
+            if increasing == True:
+                accumulator += 1
+                increasing_substrings.append((i,j))
+                continue
+            else:
+                break
+    """Finding longest increasing substring"""
+    lengths = []
+    for i in increasing_substrings:
+        lengths.append(i[1] - i[0])
+    longest = increasing_substrings[lengths.index(max(lengths))]
+    best = list(range(longest[0], longest[1]))
+    # best = [x[i] for i in best_indexes], to get the values in the longest increasing subsequence
     return best
+
+print(liseq([12, 45, 32, 65, 78, 23, 35, 45, 57]))
